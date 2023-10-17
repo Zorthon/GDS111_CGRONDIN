595c87c53523bfcd9866516f38a2e55492daaeb1
Grondin <cjgrondin@email.neit.edu>
2023-10-17 09:25:03 -0400

    Week 3 B

---
+++ Week3b.js
@@ -0,0 +1,18 @@
+console.log("i'm inside Week3 B JavaScript file.");
+
+var ammo = 100;
+console.log(ammo);
+console.log("ammo = " + ammo);
+
+ammo = prompt("Type in the number of ammo: ");
+console.log("The new entered ammo = " + ammo);
+
+var a = 0;
+var b = 0;
+var sum = 0;
+a = prompt("Enter a value: ");
+a = Number(a);
+b = prompt("Type a second value: ");
+b = Number(b);
+sum = a + b;
+console.log("The sum of a & b is = " + sum);
