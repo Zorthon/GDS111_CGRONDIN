128eda68e099e2d70942b3d6d456a43b1d9fc68b
Zorthon <grondincam9@gmail.com>
2023-10-16 20:59:37 -0400

    quest 2

--- canvas.js
+++ canvas.js
@@ -4,6 +4,53 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
-c.fillRect(100, 100, 100, 100);
-c.fillRect(400, 100, 100, 100);
-c.fillRect(300, 300, 100, 100);
+c.fillRect(1200, 1100, 100, 100);
+c.fillRect(1200, 1000, 100, 100);
+c.fillRect(1200, 900, 100, 100);
+c.fillRect(1200, 900, 200, 100);
+c.fillRect(1200, 900, 300, 100);
+c.fillRect(1200, 900, 400, 100);
+c.fillRect(1200, 900, 500, 100);
+c.fillRect(1200, 900, -100, 100);
+c.fillRect(1200, 900, -200, 100);
+c.fillRect(1200, 900, -300, 100);
+c.fillRect(1200, 900, -400, 100);
+c.fillRect(1200, 800, 100, 100);
+c.fillRect(1200, 800, -100, 100);
+c.fillRect(1200, 800, 200, 100);
+c.fillRect(1200, 800, 300, 100);
+c.fillRect(1200, 800, -200, 100);
+c.fillRect(1200, 800, 400, 100);
+c.fillRect(1200, 800, -300, 100);
+c.fillRect(1200, 700, 100, 100);
+c.fillRect(1200, 700, 200, 100);
+c.fillRect(1200, 700, 300, 100);
+c.fillRect(1200, 700, -100, 100);
+c.fillRect(1200, 700, -200, 100);
+c.fillRect(1200, 600, 100, 100);
+c.fillRect(1200, 600, -100, 100);
+c.fillRect(1200, 600, 200, 100);
+c.fillRect(1200, 500, 100, 100);
+
+
+
+
+c.fillRect(1200, 500, 100, 100);
+c.fillRect(1200, 500, -100, 100);
+c.fillRect(1200, 500, 200, 100);
+c.fillRect(1200, 500, 300, 100);
+c.fillRect(1200, 500, -200, 100);
+c.fillRect(1200, 500, 400, 100);
+c.fillRect(1200, 500, -300, 100);
+c.fillRect(1200, 400, 100, 100);
+c.fillRect(1200, 400, 200, 100);
+c.fillRect(1200, 400, 300, 100);
+c.fillRect(1200, 400, -100, 100);
+c.fillRect(1200, 400, -200, 100);
+c.fillRect(1200, 300, 100, 100);
+c.fillRect(1200, 300, -100, 100);
+c.fillRect(1200, 300, 200, 100);
+c.fillRect(1200, 200, 100, 100);
+
+
+
