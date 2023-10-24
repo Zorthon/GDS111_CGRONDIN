204362bba3af3bde4c80c95a4d05c7a7bd12d9b3
Grondin <cjgrondin@email.neit.edu>
2023-10-24 09:52:59 -0400

    Week4b

---
+++ Week4b.js
@@ -0,0 +1,16 @@
+let  age = 18;
+age = age + 1;
+console.log("You are now " + age + " years old.");
+
+age++;
+console.log("You are now " + age + " years old.");
+
+let ammo = 6;
+ammo = ammo - 1;
+ammo--;
+console.log("Ammo left in chamber: " + ammo);
+
+let balance = 0;
+balance = balance + 4.18;
+balance = balance - .50;
+console.log("you pay: " + balance);
