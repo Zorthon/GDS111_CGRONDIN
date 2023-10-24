e2822a3c0907527cc58c40217587ab592203f960
Grondin <cjgrondin@email.neit.edu>
2023-10-24 10:47:59 -0400

    Week4d

---
+++ Week4d.js
@@ -0,0 +1,22 @@
+for(let i = 0; i < 5; i++) {
+    console.log(i)
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log("Are we there yet?");
+}
+
+let text = "";
+let i = 0;
+do {
+    text = text + 1 + "<br>";
+    i++;
+}
+while(i < 5);
+console.log(text);
+
+let j = 0;
+while (j<3) {
+    console.log("The number is " + j);
+    j++;
+}
