f7763a04960e3dc1d87f19653cd947a1b128602d
Grondin <cjgrondin@email.neit.edu>
2023-10-24 09:59:18 -0400

    Week4b

--- Week4b.js
+++ Week4b.js
@@ -1,16 +1,16 @@
 let  age = 18;
-age = age + 1;
+age = age + 1; //long hand notation
 console.log("You are now " + age + " years old.");
 
-age++;
+age++; //shothand notation
 console.log("You are now " + age + " years old.");
 
 let ammo = 6;
-ammo = ammo - 1;
-ammo--;
+ammo = ammo - 1; //long hand
+ammo--;          //short hand    
 console.log("Ammo left in chamber: " + ammo);
 
 let balance = 0;
-balance = balance + 4.18;
-balance = balance - .50;
+balance = balance + 4.18; //price of milk
+balance = balance - .50;  //discount duw to coupon
 console.log("you pay: " + balance);
