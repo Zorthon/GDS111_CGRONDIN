48bc1597dd49ee4425e0c85868c87fd010d5ee55
Grondin <cjgrondin@email.neit.edu>
2023-11-07 08:46:54 -0500

    week6_1

--- Week6_1.js
+++ Week6_1.js
@@ -1,13 +1,13 @@
-const playerArr =  ["Mario", "Luigi", "Peach"];
-const pwdArr = ["diamond", "emerald", "gold"];
+const playerArr =  ["Mario", "Luigi", "Peach"]; //IDs
+const pwdArr = ["diamond", "emerald", "gold"]; //passwords
 
 let playerID = prompt("Enter player ID: ");
 let password = prompt("Type password: ");
 
-let validUser = false;
-for(let i=0; i<playerArr.length; i++) {
+let validUser = false; // a boolean
+for(let i=0; i<playerArr.length; i++) { //note .length
     if (playerID == playerArr[i] && password == pwdArr[i]) {
-        validUser = true;
+        validUser = true; //flip the boolean
     }
 }
 
